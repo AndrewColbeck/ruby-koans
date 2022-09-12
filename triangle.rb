e059030e5bba9ab4feb00a6923088d18a72eb77f
AndrewColbeck <andrewcolbeck@gmail.com>
2022-09-12 21:26:13 +1000

    More koan magic
    Signed-off-by: AndrewColbeck <andrewcolbeck@gmail.com>

--- triangle.rb
+++ triangle.rb
@@ -14,11 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if (a <= 0) || (b <= 0) || (c <= 0)
+  raise TriangleError if (a + b <= c) || (a + c <= b) || (b + c <= a)
+
   return :equilateral if (a == b) && (a == c)
-
   return :isosceles if (a == b) || (b == c) || (c == a)
+  return :scalene if (a != b) && (b != c) && (c != a)
 
-  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
